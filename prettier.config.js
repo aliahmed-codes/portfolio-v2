465c0a5b32dc7301fdbcb8c080dc1d3b710f654a
Ali Ahmed <aliahmed.dev1@gmail.com>
2026-02-12 01:54:20 +0500

    Upgrade to Gatsby 5 and Node 18+
    - Updated Gatsby from v3 to v5
    - Updated React from v17 to v18
    - Updated all gatsby-* plugins to v5
    - Updated styled-components from v5 to v6
    - Replaced gatsby-plugin-google-analytics with gatsby-plugin-google-gtag
    - Fixed GraphQL queries for Gatsby 5 compatibility
    - Fixed gatsby-node.js to pass slug in page context
    - Updated Node engine requirement to >=18.0.0

--- prettier.config.js
+++ prettier.config.js
@@ -1,1 +1,12 @@
-module.exports = require('@upstatement/prettier-config');
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  printWidth: 100,
+  proseWrap: 'always',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
